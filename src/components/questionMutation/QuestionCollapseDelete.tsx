92fb86e448b5b36094b76fe45bc5e22aec1fff99
Adiul Alam Adil <adiulalam@gmail.com>
2024-07-18 18:47:44 +0100

    fix: tooltip for question action

--- src/components/questionMutation/QuestionCollapseDelete.tsx
+++ src/components/questionMutation/QuestionCollapseDelete.tsx
@@ -1,10 +1,25 @@
 import { Button } from "../ui/Button";
 import { Trash2 } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/Tooltip";
 
 export const QuestionCollapseDelete = () => {
   return (
-    <Button variant="destructive" size="sm" className="p-1 sm:p-2">
-      <Trash2 />
-    </Button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="destructive" size="sm" className="p-1 sm:p-2">
+            <Trash2 />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Delete question</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
--- src/components/questionMutation/QuestionCollapseTrigger.tsx
+++ src/components/questionMutation/QuestionCollapseTrigger.tsx
@@ -1,15 +1,30 @@
 import { CollapsibleTrigger } from "../ui/Collapsible";
 import { Button } from "../ui/Button";
 import { ChevronDown, ChevronUp } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/Tooltip";
 
 export const QuestionCollapseTrigger = ({ isOpen }: { isOpen: boolean }) => {
   const Icon = isOpen ? ChevronUp : ChevronDown;
 
   return (
-    <CollapsibleTrigger asChild>
-      <Button variant="outline" size="sm" className="p-1 sm:p-2">
-        <Icon className="size-4" />
-      </Button>
-    </CollapsibleTrigger>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <CollapsibleTrigger asChild>
+            <Button variant="outline" size="sm" className="p-1 sm:p-2">
+              <Icon className="size-4" />
+            </Button>
+          </CollapsibleTrigger>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>{isOpen ? "Collapse" : "Expand"}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
