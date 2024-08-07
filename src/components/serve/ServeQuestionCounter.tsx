bf6875cc4780291f6de7b39511937d1064613665
Adiul Alam Adil <adiulalam@gmail.com>
2024-08-08 00:22:51 +0100

    fix: countdown to submit answer

--- src/components/serve/ServeQuestionCounter.tsx
+++ src/components/serve/ServeQuestionCounter.tsx
@@ -1,14 +1,38 @@
-import { useCountDown, useCurrentQuestion } from "@/hooks";
+import { useCountDown, useCurrentQuestion, useQuizSession } from "@/hooks";
 import { H1 } from "../ui/Typography";
+import { api } from "@/utils/api";
+import { useCallback } from "react";
 
 export const ServeQuestionCounter = () => {
-  const { currentQuestion } = useCurrentQuestion();
+  const { id } = useQuizSession();
+  const {
+    currentQuestion,
+    setCurrentQuestionId,
+    setShowSubmission,
+    currentQuestionId,
+  } = useCurrentQuestion();
 
-  const counter = useCountDown({ start: currentQuestion!.countdown, callback });
+  const { mutate } = api.quizSession.updateSessionQuestion.useMutation({
+    onSuccess: (data) => {
+      setCurrentQuestionId(data.currentQuestionId);
+      setShowSubmission(data.showSubmission);
+    },
+  });
 
-  function callback() {
-    console.log("first");
-  }
+  const callback = useCallback(() => {
+    if (currentQuestionId) {
+      mutate({
+        id,
+        showSubmission: true,
+        currentQuestionId: currentQuestionId,
+      });
+    }
+  }, [currentQuestionId, id, mutate]);
+
+  const counter = useCountDown({
+    start: currentQuestion ? currentQuestion.countdown : 30,
+    callback,
+  });
 
   return (
     <div className="flex size-16 items-center justify-center rounded-full bg-muted/80 sm:size-32">
