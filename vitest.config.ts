7404b5c09cb10cbe514752f95f5018bf24618077
Adiul Alam Adil <adiulalam@gmail.com>
2024-08-19 21:51:35 +0100

    test: add jests and vitests template

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { fileURLToPath } from "url";
+import { configDefaults, defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    exclude: [...configDefaults.exclude, "**/__tests__/**"],
+    alias: {
+      "@/": fileURLToPath(new URL("./src/", import.meta.url)),
+    },
+  },
+});
