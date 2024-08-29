00333c75aeda1f3d3c989d05277e9ef0a1813c72
Adiul Alam Adil <adiulalam@gmail.com>
2024-08-29 20:30:51 +0100

    refactor: remove commented out code

--- src/pages/join/[id].tsx
+++ src/pages/join/[id].tsx
@@ -1,7 +1,3 @@
-// import { authOptions } from "@/server/auth";
-// import { getSessionNameHandler } from "@/server/controller/quizSession.controller";
-// import type { GetServerSidePropsContext } from "next";
-// import { getServerSession } from "next-auth";
 import { JoinContainer } from "@/components/join";
 import Head from "next/head";
 
@@ -18,31 +14,5 @@ const Join = () => {
     </>
   );
 };
-
-// todo: fix this shit right here
-// export async function getServerSideProps(context: GetServerSidePropsContext) {
-//   try {
-//     const id = context.query.id;
-
-//     const userSession = await getServerSession(
-//       context.req,
-//       context.res,
-//       authOptions,
-//     );
-
-//     if (!userSession?.user?.id || typeof id !== "string") {
-//       return { redirect: { destination: "/", permanent: false } };
-//     }
-
-//     const quizSession = await getSessionNameHandler({
-//       input: { roomName: id },
-//     });
-
-//     return { props: { userSession, quizSession } };
-//   } catch (error) {
-//     console.error(error);
-//     return { redirect: { destination: "/", permanent: false } };
-//   }
-// }
 
 export default Join;
