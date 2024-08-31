966e5146039a97da1f0167370ff83bf6779ab0d0
Adiul Alam Adil <adiulalam@gmail.com>
2024-08-31 23:55:29 +0100

    test: refactor to use classes

--- __tests__/e2e/create.spec.tsx
+++ __tests__/e2e/create.spec.tsx
@@ -1,90 +1,101 @@
 import { expect, test } from "@playwright/test";
-import {
-  addAnswer,
-  addOrUpdateQuestion,
-  addOrUpdateQuiz,
-  createLocators,
-} from "../fixtures/create";
-import { findLocatorIndex } from "../setup/function";
+import { Create } from "__tests__/fixtures/create";
 
-test.beforeEach(async ({ page }) => {
-  await page.goto("/create");
-});
-
+// Test suite for the "Create Page" functionality
 test.describe("Create Page", () => {
+  let quizPage: Create;
+
+  // Before each test, instantiate the QuizPage and navigate to the create page
+  test.beforeEach(async ({ page }) => {
+    quizPage = new Create(page);
+    await page.goto("/create");
+  });
+
+  // Test to verify redirection from the base URL to the create page
   test("Redirect from base url", async ({ page }) => {
     await page.goto("/");
 
+    // Click on the button to create a new room and verify redirection
     await page.getByTestId("create-room-button").click();
-
     await page.waitForURL(/create/);
-
     await expect(page).toHaveTitle(/Create/);
   });
 
-  test("Create quiz", async ({ page }) => {
-    const {
-      buttonQuizNextStep,
-      buttonRestartQuiz,
-      closeDialog,
-      headerQuizTitle,
-    } = createLocators(page);
-    await addOrUpdateQuiz(page, false, "quiz 1", false);
+  // Test to create a new quiz, add questions and answers
+  test("Create quiz", async () => {
+    // Create a new quiz with the title "quiz 1"
+    await quizPage.addOrUpdateQuiz(false, "quiz 1", false);
 
-    await buttonQuizNextStep.click();
+    // Navigate to the next step (question creation)
+    await quizPage.buttonQuizNextStep.click();
 
-    await addOrUpdateQuestion(page, false, "question 1", -1);
-    await addAnswer(page, "question 1 answer 1 false", false);
-    await addAnswer(page, "question 1 answer 2 true", true);
+    // Add the first question and two answers
+    await quizPage.addOrUpdateQuestion(false, "question 1", -1);
+    await quizPage.addOrUpdateAnswer("q 1 a 1 false", false, false, -1, -1);
+    await quizPage.addOrUpdateAnswer("q 1 a 2 true", true, false, -1, -1);
 
-    await addOrUpdateQuestion(page, false, "question 2", -1);
-    await addAnswer(page, "question 2 answer 2 true", true);
+    // Add the second question and one answer
+    await quizPage.addOrUpdateQuestion(false, "question 2", -1);
+    await quizPage.addOrUpdateAnswer("q 2 a 1 true", true, false, -1, -1);
 
-    await buttonQuizNextStep.click();
-    await expect(buttonRestartQuiz).toBeVisible();
+    // Complete the quiz creation and validate the creation process
+    await quizPage.buttonQuizNextStep.click();
+    await expect(quizPage.buttonRestartQuiz).toBeVisible();
 
-    await closeDialog.click();
+    // Close the quiz dialog and verify the quiz title is visible on the main page
+    await quizPage.closeDialog.click();
     await expect(
-      headerQuizTitle.filter({ hasText: "quiz 1" }).first(),
+      quizPage.headerQuizTitle.filter({ hasText: "quiz 1" }).first(),
     ).toBeVisible();
   });
 
-  test("Update quiz", async ({ page }) => {
-    const {
-      buttonMenuQuiz,
-      quizCard,
-      headerQuestionTitle,
-      buttonQuizNextStep,
-    } = createLocators(page);
+  // Test to update an existing quiz
+  test("Update quiz", async () => {
+    // Find the quiz with the title "quiz 1" and open the menu to edit it
+    const quiz = quizPage.quizCard.filter({ hasText: "quiz 1" }).first();
+    await quiz.locator(quizPage.buttonMenuQuiz).click();
 
-    const quiz = quizCard.filter({ hasText: "quiz 1" }).first();
-    await quiz.locator(buttonMenuQuiz).click();
+    // Update the quiz title to "quiz 1 updated"
+    await quizPage.addOrUpdateQuiz(true, "quiz 1 updated", false);
 
-    await addOrUpdateQuiz(page, true, "quiz 1 updated", false);
+    // Navigate to the next step (question updating)
+    await quizPage.buttonQuizNextStep.click();
+    await expect(quizPage.headerQuestionTitle.nth(0)).toBeVisible();
 
-    await buttonQuizNextStep.click();
-    await expect(headerQuestionTitle.nth(0)).toBeVisible();
-
-    const questionOneIndex = await findLocatorIndex(
-      headerQuestionTitle,
-      "question 1",
-    );
-    await addOrUpdateQuestion(
-      page,
+    // Update the first question and its answers
+    const qOneIndex = 0;
+    await quizPage.addOrUpdateQuestion(true, "question 1 updated", qOneIndex);
+    await quizPage.addOrUpdateAnswer(
+      "q 1 a 1 false updated",
       true,
-      "question 1 updated",
-      questionOneIndex,
+      true,
+      qOneIndex,
+      0,
+    );
+    await quizPage.addOrUpdateAnswer(
+      "q 1 a 2 true updated",
+      true,
+      true,
+      qOneIndex,
+      1,
     );
 
-    const questionTwoIndex = await findLocatorIndex(
-      headerQuestionTitle,
-      "question 1",
-    );
-    await addOrUpdateQuestion(
-      page,
+    // Update the second question and its answer
+    const qTwoIndex = 1;
+    await quizPage.addOrUpdateQuestion(true, "question 2 updated", qTwoIndex);
+    await quizPage.addOrUpdateAnswer(
+      "q 2 a 1 true updated",
       true,
-      "question 2 updated",
-      questionTwoIndex,
+      true,
+      qTwoIndex,
+      0,
     );
+
+    // Complete the quiz update and validate the update process
+    await quizPage.buttonQuizNextStep.click();
+    await expect(quizPage.quizDialog).toBeHidden();
+    await expect(
+      quizPage.headerQuizTitle.filter({ hasText: "quiz 1 updated" }).first(),
+    ).toBeVisible();
   });
 });
--- __tests__/fixtures/create.ts
+++ __tests__/fixtures/create.ts
@@ -1,199 +1,244 @@
-import { type Page, expect } from "@playwright/test";
+import { type Page, type Locator, expect } from "@playwright/test";
 
-// Function to create locators
-export const createLocators = (page: Page) => {
-  return {
-    // view quiz
-    quizItems: page.getByTestId("quiz-items"),
-    quizCard: page.getByTestId("quiz-card"),
-    headerQuizTitle: page.getByTestId("header-quiz-title"),
-    quizDialog: page.getByTestId("quiz-dialog"),
-    closeDialog: page.getByTestId("button-close-dialog"),
+export class Create {
+  // View quiz locators
+  private readonly quizItems: Locator;
+  public readonly quizCard: Locator;
+  public readonly headerQuizTitle: Locator;
+  public readonly quizDialog: Locator;
+  public readonly closeDialog: Locator;
 
-    // create quiz
-    buttonCreateQuiz: page.getByTestId("button-create-quiz"),
-    buttonRestartQuiz: page.getByTestId("button-restart-quiz"),
+  // Create quiz locators
+  private readonly buttonCreateQuiz: Locator;
+  public readonly buttonRestartQuiz: Locator;
 
-    // Quiz - Menu update
-    buttonMenuQuiz: page.getByTestId("button-menu-quiz"),
-    menuItemEdit: page.getByTestId("menu-item-edit"),
-    menuItemFavourite: page.getByTestId("menu-item-favourite"),
-    menuItemStatus: page.getByTestId("menu-item-status"),
+  // Quiz menu update locators
+  public readonly buttonMenuQuiz: Locator;
+  private readonly menuItemEdit: Locator;
+  private readonly menuItemFavourite: Locator;
+  private readonly menuItemStatus: Locator;
+  private readonly menuItemDelete: Locator;
+  private readonly buttonDeleteContinue: Locator;
+  private readonly buttonDeleteCancel: Locator;
 
-    // Quiz - Menu update
-    menuItemDelete: page.getByTestId("menu-item-delete"),
-    buttonDeleteContinue: page.getByTestId("button-delete-continue"),
-    buttonDeleteCancel: page.getByTestId("button-delete-cancel"),
+  // Quiz mutation step locators
+  private readonly inputQuizTitle: Locator;
+  private readonly switchQuizFavourite: Locator;
 
-    // mutation - quiz step
-    inputQuizTitle: page.getByTestId("input-quiz-title"),
-    switchQuizFavourite: page.getByTestId("switch-quiz-favourite"),
+  // View question step locators
+  private readonly questionItems: Locator;
+  private readonly questionCard: Locator;
 
-    // view - question step
-    questionItems: page.getByTestId("question-items"),
-    questionCard: page.getByTestId("question-card"),
+  // Question mutation step locators
+  private readonly buttonCreateQuestion: Locator;
+  private readonly buttonToggleQuestion: Locator;
+  private readonly buttonGripQuestion: Locator;
+  public readonly headerQuestionTitle: Locator;
+  private readonly inputQuestionTitle: Locator;
+  private readonly buttonDeleteQuestion: Locator;
 
-    // mutation - question step
-    buttonCreateQuestion: page.getByTestId("button-create-question"),
-    buttonToggleQuestion: page.getByTestId("button-toggle-question"), // Expander button
-    buttonGripQuestion: page.getByTestId("button-grip-question"), // Drag and drop button
+  // View answer step locators
+  private readonly answerCard: Locator;
 
-    headerQuestionTitle: page.getByTestId("header-question-title"),
-    inputQuestionTitle: page.getByTestId("input-question-title"),
+  // Answer mutation step locators
+  private readonly buttonCreateAnswer: Locator;
+  private readonly buttonGripAnswer: Locator;
+  private readonly inputAnswerTitle: Locator;
+  private readonly switchAnswerCorrect: Locator;
 
-    buttonDeleteQuestion: page.getByTestId("button-delete-question"),
+  // Footer navigation locators
+  public readonly buttonQuizNextStep: Locator;
+  private readonly buttonQuizPrevStep: Locator;
 
-    // view - answer step
-    answerCard: page.getByTestId("answer-card"),
+  constructor(public readonly page: Page) {
+    // Initialize locators for various quiz elements
+    this.quizItems = this.page.getByTestId("quiz-items");
+    this.quizCard = this.page.getByTestId("quiz-card");
+    this.headerQuizTitle = this.page.getByTestId("header-quiz-title");
+    this.quizDialog = this.page.getByTestId("quiz-dialog");
+    this.closeDialog = this.page.getByTestId("button-close-dialog");
 
-    buttonCreateAnswer: page.getByTestId("button-create-answer"),
-    buttonGripAnswer: page.getByTestId("button-grip-answer"),
+    this.buttonCreateQuiz = this.page.getByTestId("button-create-quiz");
+    this.buttonRestartQuiz = this.page.getByTestId("button-restart-quiz");
 
-    // mutation - answer step
-    inputAnswerTitle: page.getByTestId("input-answer-title"),
-    switchAnswerCorrect: page.getByTestId("switch-answer-correct"),
+    this.buttonMenuQuiz = this.page.getByTestId("button-menu-quiz");
+    this.menuItemEdit = this.page.getByTestId("menu-item-edit");
+    this.menuItemFavourite = this.page.getByTestId("menu-item-favourite");
+    this.menuItemStatus = this.page.getByTestId("menu-item-status");
+    this.menuItemDelete = this.page.getByTestId("menu-item-delete");
+    this.buttonDeleteContinue = this.page.getByTestId("button-delete-continue");
+    this.buttonDeleteCancel = this.page.getByTestId("button-delete-cancel");
 
-    buttonDeleteAnswer: page.getByTestId("button-delete-answer"),
+    this.inputQuizTitle = this.page.getByTestId("input-quiz-title");
+    this.switchQuizFavourite = this.page.getByTestId("switch-quiz-favourite");
 
-    // mutation - footer
-    buttonQuizNextStep: page.getByTestId("button-quiz-next-step"),
-    buttonQuizPrevStep: page.getByTestId("button-quiz-prev-step"),
-  };
-};
+    this.questionItems = this.page.getByTestId("question-items");
+    this.questionCard = this.page.getByTestId("question-card");
 
-// Function to add or update a new quiz
-export const addOrUpdateQuiz = async (
-  page: Page,
-  isUpdate: boolean,
-  text: string,
-  setFavourite: boolean,
-) => {
-  const {
-    buttonCreateQuiz,
-    quizDialog,
-    inputQuizTitle,
-    switchQuizFavourite,
-    menuItemEdit,
-  } = createLocators(page);
+    this.buttonCreateQuestion = this.page.getByTestId("button-create-question");
+    this.buttonToggleQuestion = this.page.getByTestId("button-toggle-question");
+    this.buttonGripQuestion = this.page.getByTestId("button-grip-question");
+    this.headerQuestionTitle = this.page.getByTestId("header-question-title");
+    this.inputQuestionTitle = this.page.getByTestId("input-question-title");
+    this.buttonDeleteQuestion = this.page.getByTestId("button-delete-question");
 
-  if (isUpdate) {
-    // click on 'Edit' from menu
-    await menuItemEdit.click();
-  } else {
-    // click on 'Add new quiz'
-    await buttonCreateQuiz.click();
+    this.answerCard = this.page.getByTestId("answer-card");
+
+    this.buttonCreateAnswer = this.page.getByTestId("button-create-answer");
+    this.buttonGripAnswer = this.page.getByTestId("button-grip-answer");
+    this.inputAnswerTitle = this.page.getByTestId("input-answer-title");
+    this.switchAnswerCorrect = this.page.getByTestId("switch-answer-correct");
+
+    this.buttonQuizNextStep = this.page.getByTestId("button-quiz-next-step");
+    this.buttonQuizPrevStep = this.page.getByTestId("button-quiz-prev-step");
   }
 
-  await expect(quizDialog).toBeVisible();
+  /**
+   * Function to add or update a quiz.
+   * @param isUpdate - Whether this is an update operation.
+   * @param text - The quiz title.
+   * @param setFavourite - Whether to set the quiz as a favorite.
+   */
+  async addOrUpdateQuiz(
+    isUpdate: boolean,
+    text: string,
+    setFavourite: boolean,
+  ) {
+    if (isUpdate) {
+      // Click on 'Edit' from the quiz menu
+      await this.menuItemEdit.click();
+    } else {
+      // Click on 'Add new quiz'
+      await this.buttonCreateQuiz.click();
+    }
+    // Ensure that the quiz dialog is visible
+    await expect(this.quizDialog).toBeVisible();
 
-  await inputQuizTitle.fill(text);
-  await expect(inputQuizTitle).toHaveValue(text);
+    // Fill in the quiz title
+    await this.inputQuizTitle.fill(text);
+    await expect(this.inputQuizTitle).toHaveValue(text);
 
-  if (setFavourite) {
-    await switchQuizFavourite.click();
-    await expect(switchQuizFavourite).toHaveAttribute("aria-checked", "true");
-    await expect(switchQuizFavourite).toHaveAttribute("data-state", "checked");
-  } else {
-    await expect(switchQuizFavourite).toHaveAttribute("aria-checked", "false");
-    await expect(switchQuizFavourite).toHaveAttribute(
-      "data-state",
-      "unchecked",
+    // Set the quiz as a favorite if required
+    if (setFavourite) {
+      await this.switchQuizFavourite.click();
+      await expect(this.switchQuizFavourite).toHaveAttribute(
+        "aria-checked",
+        "true",
+      );
+      await expect(this.switchQuizFavourite).toHaveAttribute(
+        "data-state",
+        "checked",
+      );
+    } else {
+      await expect(this.switchQuizFavourite).toHaveAttribute(
+        "aria-checked",
+        "false",
+      );
+      await expect(this.switchQuizFavourite).toHaveAttribute(
+        "data-state",
+        "unchecked",
+      );
+    }
+  }
+
+  /**
+   * Function to add or update a question.
+   * @param isUpdate - Whether this is an update operation.
+   * @param text - The question text.
+   * @param questionPosition - The position of the question in the list.
+   */
+  async addOrUpdateQuestion(
+    isUpdate: boolean,
+    text: string,
+    questionPosition: number,
+  ) {
+    if (!isUpdate) {
+      // Get the current number of questions
+      const totalQuestion = await this.questionCard.count();
+      // Click on 'Add new question'
+      await this.buttonCreateQuestion.click();
+      // Wait for a new question to be added
+      await expect(this.questionCard).toHaveCount(totalQuestion + 1);
+    }
+
+    // Select the nth question locator
+    const questionToggle = this.buttonToggleQuestion.nth(questionPosition);
+    const questionTitle = this.headerQuestionTitle.nth(questionPosition);
+
+    // Expand the question section
+    await questionToggle.click();
+    await expect(questionToggle).toHaveAttribute("aria-expanded", "true");
+
+    // Fill in the question title
+    await this.inputQuestionTitle.fill(text);
+    await this.inputQuestionTitle.blur();
+    await expect(questionTitle).toHaveText(text);
+
+    // Collapse the question section
+    await questionToggle.click();
+    await expect(questionToggle).toHaveAttribute("aria-expanded", "false");
+  }
+
+  /**
+   * Function to add or update an answer.
+   * @param text - The answer text.
+   * @param isCorrect - Whether this answer is marked as correct.
+   * @param isUpdate - Whether this is an update operation.
+   * @param questionPosition - The position of the question in the list.
+   * @param answerPosition - The position of the answer in the list.
+   */
+  async addOrUpdateAnswer(
+    text: string,
+    isCorrect: boolean,
+    isUpdate: boolean,
+    questionPosition: number,
+    answerPosition: number,
+  ) {
+    // Select the nth question expander locator
+    const questionToggle = this.buttonToggleQuestion.nth(questionPosition);
+
+    // Expand the question section
+    await questionToggle.click();
+    await expect(questionToggle).toHaveAttribute("aria-expanded", "true");
+
+    if (!isUpdate) {
+      // Get the current number of answers
+      const totalAnswer = await this.answerCard.count();
+      // Click on 'Add new answer'
+      await this.buttonCreateAnswer.click();
+      // Wait for a new answer to be added
+      await expect(this.answerCard).toHaveCount(totalAnswer + 1);
+    }
+
+    // Select the nth answer locator
+    const answerTitle = this.inputAnswerTitle.nth(answerPosition);
+    const answerCorrectToggle = this.switchAnswerCorrect.nth(answerPosition);
+
+    // Fill in the answer title
+    await answerTitle.fill(text);
+    await answerTitle.blur();
+    await expect(answerTitle).toHaveAttribute("data-value", text);
+
+    // Set the correct answer if required
+    const dataState = await answerCorrectToggle.getAttribute("data-state");
+    if (isCorrect && dataState === "unchecked") {
+      await answerCorrectToggle.click();
+    } else if (!isCorrect && dataState === "checked") {
+      await answerCorrectToggle.click();
+    }
+
+    // Validate the correct answer state
+    await expect(answerCorrectToggle).toHaveAttribute(
+      "aria-checked",
+      isCorrect ? "true" : "false",
     );
+    await expect(answerCorrectToggle).toHaveAttribute(
+      "data-state",
+      isCorrect ? "checked" : "unchecked",
+    );
+
+    // Collapse the question section
+    await questionToggle.click();
+    await expect(questionToggle).toHaveAttribute("aria-expanded", "false");
   }
-};
-
-// Function to add a new question
-export const addOrUpdateQuestion = async (
-  page: Page,
-  isUpdate: boolean,
-  text: string,
-  questionPosition: number,
-) => {
-  const {
-    buttonCreateQuestion,
-    buttonToggleQuestion,
-    inputQuestionTitle,
-    headerQuestionTitle,
-    questionCard,
-  } = createLocators(page);
-
-  if (!isUpdate) {
-    const totalQuestion = await questionCard.count();
-    // click on 'Add new question'
-    await buttonCreateQuestion.click();
-    // wait for new question to be added
-    await expect(questionCard).toHaveCount(totalQuestion + 1);
-  }
-
-  // select the latest question locator
-  const lastButtonToggle = buttonToggleQuestion.nth(questionPosition);
-  const lastInputQuestion = inputQuestionTitle.nth(questionPosition);
-  const lastHeaderTitle = headerQuestionTitle.nth(questionPosition);
-
-  // click to expand
-  await lastButtonToggle.click();
-  await expect(lastButtonToggle).toHaveAttribute("aria-expanded", "true");
-
-  // fill in the question
-  await lastInputQuestion.fill(text);
-
-  // Check if the question name has been saved
-  await lastInputQuestion.blur();
-  await expect(lastHeaderTitle).toHaveText(text);
-
-  // click on collapse
-  await lastButtonToggle.click();
-  await expect(lastButtonToggle).toHaveAttribute("aria-expanded", "false");
-};
-
-// Function to add a new answer
-export const addAnswer = async (
-  page: Page,
-  text: string,
-  isCorrect: boolean,
-) => {
-  const {
-    buttonCreateAnswer,
-    buttonToggleQuestion,
-    switchAnswerCorrect,
-    inputAnswerTitle,
-    answerCard,
-  } = createLocators(page);
-  // select the latest answer locator
-  const lastButtonToggle = buttonToggleQuestion.last();
-  const lastInputAnswer = inputAnswerTitle.last();
-  const lastCorrectToggle = switchAnswerCorrect.last();
-
-  // click to expand
-  await lastButtonToggle.click();
-  await expect(lastButtonToggle).toHaveAttribute("aria-expanded", "true");
-
-  const totalAnswer = await answerCard.count();
-
-  // click on 'Add new answer'
-  await buttonCreateAnswer.click();
-
-  // wait for new answer to be added
-  await expect(answerCard).toHaveCount(totalAnswer + 1);
-
-  // fill in the answer
-  await lastInputAnswer.fill(text);
-
-  // Check if the answer name has been saved
-  await lastInputAnswer.blur();
-  await expect(lastInputAnswer).toHaveAttribute("data-value", text);
-
-  // Radio button for correct answer
-  if (isCorrect) {
-    await lastCorrectToggle.click();
-    await expect(lastCorrectToggle).toHaveAttribute("aria-checked", "true");
-    await expect(lastCorrectToggle).toHaveAttribute("data-state", "checked");
-  } else {
-    await expect(lastCorrectToggle).toHaveAttribute("aria-checked", "false");
-    await expect(lastCorrectToggle).toHaveAttribute("data-state", "unchecked");
-  }
-
-  // click on collapse
-  await lastButtonToggle.click();
-  await expect(lastButtonToggle).toHaveAttribute("aria-expanded", "false");
-};
+}
