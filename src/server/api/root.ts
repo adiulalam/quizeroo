3ab4352743d790586b45a3d6bb6437b1fbb43af9
Adiul Alam Adil <adiulalam@gmail.com>
2024-09-01 01:36:06 +0100

    chore: remove post from db

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "@/server/api/routers/post.routes";
 import {
   createCallerFactory,
   createTRPCRouter,
@@ -19,7 +18,6 @@ import { userAnswerRouter } from "./routers/userAnswer.routes";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   quiz: quizRouter,
   question: questionRouter,
   answer: answerRouter,
