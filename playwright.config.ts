ae08b6fb7ea7e06246b291ba5347166f968d1f99
Adiul Alam Adil <adiulalam@gmail.com>
2025-08-01 01:10:11 +0100

    test: add correct server

--- playwright.config.ts
+++ playwright.config.ts
@@ -52,7 +52,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run dev",
+    command: process.env.CI ? "npm run start" : "npm run dev",
     port: 3000,
     reuseExistingServer: true,
   },
